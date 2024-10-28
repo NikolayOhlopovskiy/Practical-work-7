da2a70c3e5c3d5a191584866ff9dd07e871eb1fc
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2024-10-28 20:30:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] omas = new string[5];
+            //Задание 1
+            //Console.Write("Ввыведите слово");
+            //int x = int.Parse(Console.ReadLine());
+            //int count = 0;
+            /*Решение 1*///while(count<10)
+                         //{
+                         //    Console.WriteLine("Привет");
+                         //    count ++;
+                         //}
+
+            /* Решение 2*///do
+                          //{
+                          //    Console.WriteLine("Привет");
+                          //    count++;
+                          //}
+                          //while (count < 10);
+
+            /*Решение 3*///for(int count=0;count<10;count++)
+                         //{
+                         //    Console.WriteLine("Привет");
+                         //}
+
+
+            //Задание 2
+            //Console.Write("Ввыведите натуральные числа");
+            //int x = int.Parse(Console.ReadLine());
+            //int count = 1;
+            //*Решение 1*/while(count<=10)
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //}
+
+            //* Решение 2*/do
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //}
+            //while (count <= 10) ;
+
+            /*Решение 3*/
+            //for (int count = 1; count <= 10; count++)
+            //{
+            //    Console.WriteLine(count);
+            //}
+
+
+            //Задание 3
+            //Console.Write("Ввыведите на экран в столбик первые 5 натуральных чисел в обратном порядке");
+            //int x = int.Parse(Console.ReadLine());
+            //int count = 5;
+            ///*Решение 1*/while(count>=1)
+            //{
+            //    Console.WriteLine(count);
+            //    count--;
+            //}
+
+            /* Решение 2*/
+            //do
+            //{
+            //    Console.WriteLine(count);
+            //    count--;
+            //}
+            //while (count >=1);
+
+            ///*Решение 3*/for(int count=5;count>=1;count--)
+            //{
+            //    Console.WriteLine(count);
+            //}
+
+
+            //Решение 4
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int count = 1;
+            //*Решение 1*/while(count<=n)
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //}
+
+            //* Решение 2*/do
+            //{
+            //    Console.WriteLine(count);
+            //    count++;
+            //}
+            //while (count <=n) ;
+
+            //*Решение 3*/for(int count=1;count<=n;count++)
+            //{
+            //    Console.WriteLine(count);
+            //}
+
+
+            
+            
+            Console.Read();
+            
+
+        }
+    }
+}
